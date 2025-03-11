f35f48fc96f10ff295d7725e36dd2d5a47053443
Vineet2511SRM <vs4622@srmist.edu.in>
2025-03-11 11:45:16 +0530

    INHERITANCE

--- C++/OOPS/INHERITANCE/program2.cpp
+++ C++/OOPS/INHERITANCE/program2.cpp
@@ -1,41 +1,46 @@
 #include <iostream>
-#include <string> // Correct header for std::string
+#include <string>
 
 using namespace std;
 
 class concat {
 protected:
-    string name;  // Changed to protected so derived class can access it
+    string name;
 
 public:
     concat(string value) { name = value; }
 
-    // Overloaded + operator
-    int operator+(const concat& temp) {  
-        cout<<(name + temp.name); // Concatenating 
-        return 0;
+    // Overloaded + operator for concatenation
+    string operator+(const concat& temp) {  
+        return name + temp.name; 
     }
-
- };
+};
 
 class compare : public concat {
 public:
-    // Constructor correctly initializes base class
     compare(string value) : concat(value) {}
 
-    // Inheriting operator+ from base class
+    // Overloading + for string comparison
+    bool operator+(const compare& temp) {
+        if (name == temp.name) {
+            cout << "Strings are equal." << endl;
+            return true;
+        } else {
+            cout << "Strings are not equal." << endl;
+            return false;
+        }
+    }
 };
 
 int main() {
     string val1, val2;
-    cout << "Enter the values: \n";
+    cout << "Enter two strings: \n";
     cin >> val1 >> val2;
 
     compare obj1(val1);
     compare obj2(val2);
 
-    obj1 + obj2;  // Using overloaded '+' operator
-    
+    obj1 + obj2;  // Using overloaded '+' operator for comparison
 
     return 0;
 }
