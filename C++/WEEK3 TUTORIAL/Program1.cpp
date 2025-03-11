c57e6111773810848c04266905730963c966a28f
Vineet2511SRM <vs4622@srmist.edu.in>
2025-03-11 11:40:44 +0530

    WEEK3 TUTORIAL

---
+++ C++/WEEK3 TUTORIAL/Program1.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BankAccount {
+private:
+    int accountNumber;
+    string accountHolderName;
+    float balance;
+
+public:
+    // Constructor
+    BankAccount(int accNum, const string& accHolderName, float bal) {
+        accountNumber = accNum;
+        accountHolderName = accHolderName;
+        balance = bal;
+    }
+
+    // Function to deposit money
+    void deposit(float amount) {
+        if (amount > 0) {
+            balance += amount;
+            cout << "Deposited: " << amount << endl;
+        } else {
+            cout << "Deposit amount must be positive!" << endl;
+        }
+    }
+
+    // Function to withdraw money
+    void withdraw(float amount) {
+        if (amount > 0) {
+            if (balance - amount >= 0) {
+                balance -= amount;
+                cout << "Withdrawn: " << amount << endl;
+            } else {
+                cout << "Insufficient balance to withdraw " << amount << endl;
+            }
+        } else {
+            cout << "Withdrawal amount must be positive!" << endl;
+        }
+    }
+
+    // Function to display account details
+    void display() const {
+        cout << "----------------------------------" << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Account Holder Name: " << accountHolderName << endl;
+        cout << "Balance: " << balance << endl;
+        cout << "----------------------------------" << endl;
+    }
+};
+
+int main() { 
+    int accNum;
+    string accHolderName;
+    float initialBalance;
+
+    // Taking user input for account details
+    cout << "Enter Account Number: ";
+    cin >> accNum;
+    cin.ignore();  // Ignore newline character
+    cout << "Enter Account Holder Name: ";
+    getline(cin, accHolderName);
+    cout << "Enter Initial Balance: ";
+    cin >> initialBalance;
+
+    // Creating a user-defined BankAccount object
+    BankAccount account1(accNum, accHolderName, initialBalance); 
+
+    float depositAmount, withdrawAmount;
+
+    // Performing deposit
+    cout << "Enter amount to deposit: ";
+    cin >> depositAmount;
+    account1.deposit(depositAmount);
+    
+    // Performing withdrawal
+    cout << "Enter amount to withdraw: ";
+    cin >> withdrawAmount;
+    account1.withdraw(withdrawAmount);
+    
+    // Displaying final account details
+    account1.display();
+    
+    return 0;
+}
