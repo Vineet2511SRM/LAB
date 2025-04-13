fc8ecce4c2c72412faafb28657542ac66a73fea4
Vineet2511SRM <vs4622@srmist.edu.in>
2025-04-13 19:46:36 +0530

    WEEK10 TUTORIAL

--- C++/WEEK10 TUTORIAL/Program4.cpp
+++ C++/WEEK10 TUTORIAL/Program4.cpp
@@ -3,23 +3,32 @@
 using namespace std;
 
 int main() {
-    stack<string> typedWords;
+    stack<string> typedWords; // Stack to store typed words
 
     // Typing three words
     typedWords.push("Hello");
     typedWords.push("World");
     typedWords.push("Test");
 
-    // Undo the last typed word
-    if (!typedWords.empty()) {
-        typedWords.pop();
+    // Displaying the words typed
+    cout << "Words typed:\n";
+    stack<string> temp = typedWords;  // Copy the stack to display
+    while (!temp.empty()) {
+        cout << temp.top() << endl;
+        temp.pop();
     }
 
-    // Displaying the remaining words
-    cout << "Words after undo:\n";
-    while (!typedWords.empty()) {
-        cout << typedWords.top() << endl;
-        typedWords.pop();
+    // Undo the last typed word
+    if (!typedWords.empty()) {
+        typedWords.pop(); // Removes the last typed word ("Test")
+    }
+
+    // Displaying the remaining words after undo
+    cout << "\nWords after undo:\n";
+    temp = typedWords;  // Copy the stack again to display the updated list
+    while (!temp.empty()) {
+        cout << temp.top() << endl;
+        temp.pop();
     }
 
     return 0;
